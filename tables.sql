8024d9e325d6276d5c55b38a63c293e18280aaa4
robsonbaioco <robson_baioco@hotmail.com>
2016-11-08 15:22:04 -0200

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -81,30 +81,26 @@ CREATE TABLE IF NOT EXISTS Info_Material (
 DROP TABLE IF EXISTS Material ;
 
 CREATE TABLE IF NOT EXISTS Material (
-  ID_material INT NOT NULL,
-  nome VARCHAR(45) NULL,
-  preco VARCHAR(45) NULL,
-  validade VARCHAR(45) NULL,
-  Info_Material_ID_material INT NOT NULL,
-  PRIMARY KEY (ID_material, Info_Material_ID_material),
-    FOREIGN KEY (Info_Material_ID_material)
-    REFERENCES Info_Material (ID_material)
+ID_material INT NOT NULL,
+nome VARCHAR(45) NULL,
+preco VARCHAR(45) NULL,
+validade VARCHAR(45) NULL,
+PRIMARY KEY (ID_material)
 )
 ;
-
 -- -----------------------------------------------------
 -- Table Requisicao
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS Requisicao ;
 
 CREATE TABLE IF NOT EXISTS Requisicao (
-  setor INT NULL,
-  data DATE NULL,
-  ID_req VARCHAR(45) NOT NULL,
-  Funcionario_ID_func INT NOT NULL,
-  PRIMARY KEY (ID_req),
-    FOREIGN KEY (Funcionario_ID_func)
-    REFERENCES Funcionario (ID_func)
+setor INT NULL,
+data DATE NULL,
+ID_req VARCHAR(45) NOT NULL,
+Funcionario_ID_func INT NOT NULL,
+PRIMARY KEY (ID_req),
+FOREIGN KEY (Funcionario_ID_func)
+REFERENCES Funcionario (ID_func)
 )
 ;
 
@@ -114,17 +110,16 @@ CREATE TABLE IF NOT EXISTS Requisicao (
 DROP TABLE IF EXISTS Item_lista_req ;
 
 CREATE TABLE IF NOT EXISTS Item_lista_req (
-  seq_item INT NOT NULL,
-  quantidade INT NULL,
-  Requisicao_ID_req VARCHAR(45) NOT NULL,
-  Material_ID_material INT NOT NULL,
-  Material_Info_Material_ID_material INT NOT NULL,
-  PRIMARY KEY (seq_item, Requisicao_ID_req),
-    FOREIGN KEY (Requisicao_ID_req)
-    REFERENCES Requisicao (ID_req)
+seq_item INT NOT NULL,
+quantidade INT NULL,
+Requisicao_ID_req VARCHAR(45) NOT NULL,
+Material_ID_material INT NOT NULL,
+PRIMARY KEY (seq_item, Requisicao_ID_req),
+FOREIGN KEY (Requisicao_ID_req)
+REFERENCES Requisicao (ID_req)
 ,
-    FOREIGN KEY (Material_ID_material , Material_Info_Material_ID_material)
-    REFERENCES Material (ID_material , Info_Material_ID_material)
+FOREIGN KEY (Material_ID_material)
+REFERENCES Material (ID_material )
 )
 ;
 
@@ -134,13 +129,13 @@ CREATE TABLE IF NOT EXISTS Item_lista_req (
 DROP TABLE IF EXISTS Entrada ;
 
 CREATE TABLE IF NOT EXISTS Entrada (
-  total INT NULL,
-  data DATE NULL,
-  ID_entrada VARCHAR(45) NOT NULL,
-  Fornecedor_ID_fornecedor INT NOT NULL,
-  PRIMARY KEY (ID_entrada, Fornecedor_ID_fornecedor),
-    FOREIGN KEY (Fornecedor_ID_fornecedor)
-    REFERENCES Fornecedor (ID_fornecedor)
+total INT NULL,
+data DATE NULL,
+ID_entrada VARCHAR(45) NOT NULL,
+Fornecedor_ID_fornecedor INT NOT NULL,
+PRIMARY KEY (ID_entrada),
+FOREIGN KEY (Fornecedor_ID_fornecedor)
+REFERENCES Fornecedor (ID_fornecedor)
 )
 ;
 -- -----------------------------------------------------
@@ -149,17 +144,16 @@ CREATE TABLE IF NOT EXISTS Entrada (
 DROP TABLE IF EXISTS Item_lista_ent ;
 
 CREATE TABLE IF NOT EXISTS Item_lista_ent (
-  seq_item INT NOT NULL,
-  quantidade INT NULL,
-  Entrada_ID_entrada VARCHAR(45) NOT NULL,
-  Material_ID_material INT NOT NULL,
-  Material_Info_Material_ID_material INT NOT NULL,
-  PRIMARY KEY (seq_item, Entrada_ID_entrada),
-    FOREIGN KEY (Entrada_ID_entrada)
-    REFERENCES Entrada (ID_entrada)
+seq_item INT NOT NULL,
+quantidade INT NULL,
+Entrada_ID_entrada VARCHAR(45) NOT NULL,
+Material_ID_material INT NOT NULL,
+PRIMARY KEY (seq_item, Entrada_ID_entrada),
+FOREIGN KEY (Entrada_ID_entrada)
+REFERENCES Entrada (ID_entrada)
 ,
-    FOREIGN KEY (Material_ID_material , Material_Info_Material_ID_material)
-    REFERENCES Material (ID_material , Info_Material_ID_material)
+FOREIGN KEY (Material_ID_material)
+REFERENCES Material (ID_material )
 )
 ;
 -- -----------------------------------------------------
@@ -168,10 +162,11 @@ CREATE TABLE IF NOT EXISTS Item_lista_ent (
 DROP TABLE IF EXISTS Tel_func ;
 
 CREATE TABLE IF NOT EXISTS Tel_func (
-  telefone INT NULL,
-  Funcionario_ID_func INT NOT NULL,
-    FOREIGN KEY (Funcionario_ID_func)
-    REFERENCES Funcionario (ID_func)
+telefone INT NULL,
+Funcionario_ID_func INT NOT NULL,
+PRIMARY KEY (telefone),
+FOREIGN KEY (Funcionario_ID_func)
+REFERENCES Funcionario (ID_func)
 )
 ;
 
@@ -239,17 +234,16 @@ CREATE TABLE IF NOT EXISTS Trabalha (
 DROP TABLE IF EXISTS Armazena ;
 
 CREATE TABLE IF NOT EXISTS Armazena (
-  Local_armazenamento_setor INT NOT NULL,
-  Local_armazenamento_nome VARCHAR(45) NOT NULL,
-  Material_ID_material INT NOT NULL,
-  quantidade INT NULL,
-  PRIMARY KEY (Local_armazenamento_setor, Local_armazenamento_nome, Material_ID_material),
-    FOREIGN KEY (Local_armazenamento_setor , Local_armazenamento_nome)
-    REFERENCES Local_armazenamento (setor , nome)
+Local_armazenamento_setor INT NOT NULL,
+Local_armazenamento_nome VARCHAR(45) NOT NULL,
+Material_ID_material INT NOT NULL,
+quantidade INT NULL,
+PRIMARY KEY (Local_armazenamento_setor, Local_armazenamento_nome, Material_ID_material),
+FOREIGN KEY (Local_armazenamento_setor , Local_armazenamento_nome)
+REFERENCES Local_armazenamento (setor , nome)
 ,
-
-    FOREIGN KEY (Material_ID_material)
-    REFERENCES Material (ID_material)
+FOREIGN KEY (Material_ID_material)
+REFERENCES Material (ID_material)
 )
 ;
 ALTER TABLE Fornecedor (
